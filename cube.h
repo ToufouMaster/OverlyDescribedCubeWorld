137dc0b20fa5b72f2531903f3c2191ebd612efb6
ToufouMaster <owen.kala@gmx.fr>
2025-03-19 14:25:21 +0100

    reworked some parts and removed any useless code
    Known issue: incompatible with TravelMod as it hook at the exact same address

--- cube.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "GameController.h"
-
-namespace cube
-{
-	GameController* GetGameController();
-};
-
--- globals.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-namespace cube {
-	void InitGlobals();
-	extern float* gravity;
-	extern HWND* hwnd;
-	extern bool* input_enabled;
-};
